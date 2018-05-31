e1005a8102746becda07945e306986913e5106f5
CodeDad <github email address>
2018-05-30 22:03:27 -0400

    added paths to app.js

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ app.listen(3000);
 console.log("App is running…");
 
 function response(req, res) {
-  fs.readFile(__dirname + '/index.html',
-    function (err, data) {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Failed to load the file index.html');
-      }
-      res.writeHead(200);
-      res.end(data);
-    });
+  var file = "";
+  if (req.url == "/") {
+    file = __dirname + '/index.html';
+  } else {
+    file = __dirname + req.url;
+  }
+
+  fs.readFile(file, function (err, data){
+    if (err) {
+      res.writeHead(404);
+      return res.end('Page or file note found');
+    }
+    res.writeHead(200);
+    res.end(data);
+  })
 }
+
