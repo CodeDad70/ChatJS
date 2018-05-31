49689d5f47a0275d3ac24c4aa5b4423dc3eb0306
CodeDad <github email address>
2018-05-30 21:43:20 -0400

    updated the listener to return an error if index.html is not found

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var app = require('http').createServer(response);
+var fs = require('fs');
+
 app.listen(3000);
-console.log("App running…");
+console.log("App is running…");
+
 function response(req, res) {
- res.writeHead(200);
- res.end("Hi, your server is working!");
+  fs.readFile(__dirname + '/index.html',
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Failed to load the file index.html');
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
 }
